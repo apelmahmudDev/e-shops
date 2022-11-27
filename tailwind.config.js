2f319cc0d0753e48b0816db412238fbcd84386db
Apel Mahmud <apelmahmud.dev@gmail.com>
2022-11-27 22:36:02 +0600

    📦 add flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	content: [
+		"./src/**/*.{js,jsx,ts,tsx}",
+		"node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#7480cd",
+				secondary: "#7f8d98",
+				light: "#e1eaf9",
+				"light-dark": "#b2bac5",
+			},
+		},
 	},
-	plugins: [],
+	plugins: [require("flowbite/plugin")],
 };
