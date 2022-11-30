7cc2db7de7395c5b751c37a80085fab5d3d5fb1f
Apel Mahmud <apelmahmud.dev@gmail.com>
2022-11-30 20:00:00 +0600

    product show from json file

--- src/components/ProductContainer/Product.js
+++ src/components/ProductContainer/Product.js
@@ -1,9 +1,13 @@
 import ProductCard from "./ProductCard";
+import products from "../../assets/data/productsData.json";
 
 const Product = () => {
 	return (
-		<div className="mt-4">
-			<ProductCard />
+		<div className="mt-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+			{products?.data &&
+				products.data.map((product) => (
+					<ProductCard key={product._id} product={product} />
+				))}
 		</div>
 	);
 };
